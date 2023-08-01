87625b0931759823e737910bbb401130897eaaf9
Regisjr246 <regisjr246@gmail.com>
2023-08-01 13:07:02 -0300

    exercio 10

--- app/Http/Controllers/ExercicioDez.php
+++ app/Http/Controllers/ExercicioDez.php
@@ -6,5 +6,9 @@ use Illuminate\Http\Request;
 
 class ExercicioDez extends Controller
 {
-    public function impar() { $numeros = []; for ($i = 0; $i <= 80; $i++) { if ($i % 2 ==1) array_push($numeros, $i); } return json_encode([ 'valores'=> $numeros ]); }
+    public function impar() 
+    { $numeros = [];
+         for ($i = 0; $i <= 80; $i++)
+          { if ($i % 2 ==1) array_push($numeros, $i); 
+        }  return json_encode([ 'valores'=> $numeros ]); }
 }
